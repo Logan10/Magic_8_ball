7ff434a96e908ba0ae809982a3a9d9dc1daec8d9
Kanishk Sharma <kanishk.3107@gmail.com>
2017-11-01 21:29:33 +0530

    Update

---
+++ app/src/main/java/com/example/kanishk/magic_8_ball/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kanishk.magic_8_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
